13dea5e4210b28e570f11e1005928f2bd9b1f2c0
karreemali <karreem.ali@gmail.com>
2023-08-29 14:43:01 -0400

    adding nginx and wsgi/gunicorn to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,40 @@
 #!/bin/sh
 sudo apt-get update
 
-# apache/wsgi
-sudo apt-get install apache2
-sudo apt-get install libapache2-mod-wsgi-py3
-sudo a2enmod wsgi
+# install stuff
+sudo apt update
+sudo apt install python3-pip\
+ python3-dev\
+ build-essential\
+ libssl-dev\
+ libffi-dev\
+ python3-setuptools\
+ nginx\
+ wheel -y
 
+# pull git repo
+git clone https://github.com/karreemali/AQS_Server.git
+
+# install python requirements
+cd AQS_Server
+python3 -m pip install -r- requirements.txt
+
+# enable gunicorn
+gunicorn --bind 0.0.0.0:5000 wsgi:app
+
+# enable service
+cp service.conf /etc/systemd/system/AQS_Server.service
+systemctl start AQS_Server
+systemctl enable AQS_Server
+
+# enable nginx
+cp nginx.conf /etc/nginx/sites-available/AQS_Server
+ln -s /etc/nginx/sites-available/AQS_Server /etc/nginx/sites-enabled
+nginx -t
+systemctl restart nginx
 
 # allow port 5000
-sudo apt-get install ufw
-sudo ufw enable
-sudo ufw allow 5000
+apt-get install ufw
+ufw enable
+ufw allow 5000
+ufw allow 'Nginx Full"
