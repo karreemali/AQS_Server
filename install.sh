1812a45c49be2a1f4103d1225e6c9aec1b375415
karreemali <karreem.ali@gmail.com>
2023-09-01 20:38:25 -0400

    Fixing issue with deployment: source bin/activate doesn't work

--- install.sh
+++ install.sh
@@ -1,40 +1,42 @@
 #!/bin/sh
-sudo apt-get update
 
 # install stuff
-sudo apt update
+#sudo apt update
 sudo apt install python3-pip\
  python3-dev\
  build-essential\
  libssl-dev\
  libffi-dev\
  python3-setuptools\
- nginx\
- wheel -y
+ nginx -y
 
 # pull git repo
 git clone https://github.com/karreemali/AQS_Server.git
+cd AQS_Server
+sudo chmod 755 .
 
 # install python requirements
-cd AQS_Server
-python3 -m pip install -r- requirements.txt
-
-# enable gunicorn
-gunicorn --bind 0.0.0.0:5000 wsgi:app
-
-# enable service
-cp service.conf /etc/systemd/system/AQS_Server.service
-systemctl start AQS_Server
-systemctl enable AQS_Server
-
-# enable nginx
-cp nginx.conf /etc/nginx/sites-available/AQS_Server
-ln -s /etc/nginx/sites-available/AQS_Server /etc/nginx/sites-enabled
-nginx -t
-systemctl restart nginx
+python3 -m pip install -r ~/AQS_Server/requirements.txt
 
 # allow port 5000
-apt-get install ufw
-ufw enable
-ufw allow 5000
-ufw allow 'Nginx Full"
+sudo apt-get install ufw
+sudo ufw enable
+sudo ufw allow 5000
+
+# enable gunicorn
+sudo gunicorn --bind 0.0.0.0:5000 wsgi:app
+
+# enable service
+#sudo touch /etc/systemd/system/AQS_Server.service
+sudo cp service.conf /etc/systemd/system/AQS_Server.service
+sudo systemctl start AQS_Server
+sudo systemctl enable AQS_Server
+
+# enable nginx
+sudo cp nginx.conf /etc/nginx/sites-available/AQS_Server
+sudo ln -s /etc/nginx/sites-available/AQS_Server /etc/nginx/sites-enabled
+sudo nginx -t
+sudo systemctl restart nginx
+
+# allow port nginx
+sudo ufw allow 'Nginx Full"
